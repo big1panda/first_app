590a00b870ab3b8564dab14cdc73b24e4a53e60f
big1panda <big1panda@gmail.com>
2014-04-12 21:49:43 +0300

    ebd maf

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :microposts
+  root 'static_pages#home'
+  get 'static_pages/home'
 
+  resources :microposts
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
