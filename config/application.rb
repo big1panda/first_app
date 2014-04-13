416a76fe7b45eeba66b533d9cc5cf77c1d55812e
big1panda <big1panda@gmail.com>
2014-04-13 22:51:43 +0300

    Update Gemfile.lock for Heroku

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module FirstApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.prefix     = "/new_route"
+    config.serve_static_assets = true
 
   end
 end
